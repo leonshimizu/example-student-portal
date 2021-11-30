a1d846bf6a36b574ced518d8d7157bdfe43bd536
“leonshimizu” <lmshimizu@gmail.com>
2021-11-29 18:24:42 -0800

    added code to solve n + 1 solution for cohorts

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,6 +1,10 @@
 class CohortsController < ApplicationController
-  def index
-    cohorts = Cohort.all
+  # def index # n + 1 problem
+  #   cohorts = Cohort.all
+  #   render json: cohorts, include: "users.submissions.comments"
+  # end
+  def index # n + 1 solution
+    cohorts = Cohort.includes(:users => {:submissions => :comments}).all
     render json: cohorts, include: "users.submissions.comments"
   end
 end
